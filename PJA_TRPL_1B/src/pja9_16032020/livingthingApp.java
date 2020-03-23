20f2b61edfcea47cd931c35198f26a9928dbcd28
Andi-IM <andi.irhamm@gmail.com>
2020-03-23 08:10:57 +0700

    Penambahan beberapa item yang dibutuhkan

---
+++ PJA_TRPL_1B/src/pja9_16032020/livingthingApp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pja9_16032020;
+
+/**
+ *
+ * @author Andi
+ */
+public class livingthingApp {
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.breath();
+        human.eat();
+        human.walk();
+        
+        Dog dog = new Dog();
+        dog.breath();
+        dog.eat();
+        dog.walk();
+    }
+}
