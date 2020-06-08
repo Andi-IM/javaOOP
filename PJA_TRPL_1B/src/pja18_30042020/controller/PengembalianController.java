d63f25cae9745581e80b8ebd0792724645b6d7a0
Andi Irham <Andi.irhamm@gmail.com>
2020-06-08 09:16:01 +0700

    [Andi-IM] Adding some controller

--- PJA_TRPL_1B/src/pja18_30042020/controller/PengembalianController.java
+++ PJA_TRPL_1B/src/pja18_30042020/controller/PengembalianController.java
@@ -5,9 +5,11 @@
  */
 package pja18_30042020.controller;
 
+
+import java.awt.HeadlessException;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Date;
-import pja18_30042020.dao.AnggotaDao;
 import pja18_30042020.dao.AnggotaDaoImpl;
 import pja18_30042020.dao.BukuDaoImpl;
 import pja18_30042020.dao.PeminjamanDaoImpl;
@@ -16,19 +18,26 @@ import pja18_30042020.http.Koneksi;
 import pja18_30042020.model.PeminjamanModel;
 import pja18_30042020.view.FormPengembalian;
 import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import pja18_30042020.model.AnggotaModel;
+import pja18_30042020.model.BukuModel;
 /**
  *
  * @author Andi
  */
 public class PengembalianController {
-    final private FormPengembalian view;
-    final private Koneksi k;
-    final private Connection con;
-    AnggotaDao anggotaDao;
+    private FormPengembalian view;
+    private Koneksi k;
+    private Connection con;
+    AnggotaDaoImpl anggotaDao;
+    AnggotaModel anggota;
     BukuDaoImpl bukuDao;
+    BukuModel buku;
     PeminjamanDaoImpl peminjamanDao;
     PengembalianDaoImpl pengembalian;
-    final private PeminjamanModel peminjaman;
+    private PeminjamanModel peminjaman;
     String dateNow;
     SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd");
 
@@ -36,15 +45,46 @@ public class PengembalianController {
         this.view = view;
         k = new Koneksi();
         con = k.getConnection();
-        anggotaDao = new AnggotaDaoImpl();
+        //anggotaDao.create(anggota);
         bukuDao = new BukuDaoImpl();
         peminjamanDao = new PeminjamanDaoImpl();
         pengembalian = new PengembalianDaoImpl();
         peminjaman = new PeminjamanModel();
-        dateNow = format.format(new Date());   
+        dateNow = format.format(new Date());
     }
 
     public void viewTanggal(){
         view.getTxtDikembalikan().setText(dateNow);
+    }
+    
+    public void onKeyPressKodeAnggota(){
+        try {
+            anggotaDao = new AnggotaDaoImpl();
+            anggota =  anggotaDao.getAnggota(view.getTxtKodeAnggota().getText());
+            if (anggota != null) {
+                view.getTxtNamaAnggota().setText(anggota.getNama());
+                view.getTxtKodeBuku().requestFocus();
+            } else {
+                JOptionPane.showMessageDialog(view, "Data tidak ada");
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(AnggotaController.class.getName()).log(java.util.logging.Level.SEVERE,null,e);
+        }
+    }
+    
+    public void onKeyPressKodeBuku()
+    {
+        try {
+            bukuDao = new BukuDaoImpl();
+            buku = bukuDao.getBuku(view.getTxtKodeBuku().getText());
+            if (buku!=null) {
+                view.getTxtJudulBuku().setText(buku.getJudulbuku());
+                view.getTxtTglPinjam().requestFocus();
+            } else {
+                JOptionPane.showMessageDialog(view, "Data Tidak Ada!");
+            }
+        } catch (HeadlessException | SQLException e) {
+            Logger.getLogger(AnggotaController.class.getName()).log(java.util.logging.Level.SEVERE,null,e);
+        }
     }
 }
